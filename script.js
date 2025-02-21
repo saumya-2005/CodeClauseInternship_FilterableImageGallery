2d1560b4caaba3978c174daeae0685fb14396a92
Saumya <saumyakushwaha1601@gmail.com>
2025-02-21 11:58:29 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let  list=document.querySelectorAll('.list');
+let itembox=document.querySelectorAll('.itembox');
+for(let i=0;i<list.length;i++){
+    list[i].addEventListener('click',function(){
+        for(let j=0;j<list.length;j++){
+            list[j].classList.remove('active');
+        }
+    this.classList.add('active');
+    let dataFilter=this.getAttribute('data-filter');
+    for(let k=0;k<itembox.length;k++){
+        itembox[k].classList.remove('active');
+        itembox[k].classList.add('hide');
+
+        if(itembox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+            itembox[k].classList.remove('hide');
+            itembox[k].classList.add('active');
+        }
+    }
+    })
+}
